068588a1ad418d24e1fb0a2a49ad198b0b43ce76
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-04-06 00:23:28 -0400

    Beautiful UI

--- src/Chat.py
+++ src/Chat.py
@@ -9,9 +9,9 @@ MODEL = 'llama3.2:1b'
 
 # Sends a message to the chatbot
 async def query_chatbot(state: State):
-    # await asyncio.sleep(5)
-    # return { "message": { "role": "system", "content": "Hi" } }
-    return chat(MODEL, state.chat_history)
+    await asyncio.sleep(5)
+    return { "message": { "role": "system", "content": "Hi" } }
+    # return chat(MODEL, state.chat_history)
 
 async def on_send_message(state: State) -> None:
     # Get user's question
@@ -32,10 +32,10 @@ async def on_send_message(state: State) -> None:
     # and query the LLM for a response
     prepping_question_notifier = ui.notification("Processing question...")
     prepping_question_notifier.spinner = True
-    state.chat_history.append({
-        "role": "user",
-        "content": await PromptBuilder().build_prompt(message)
-    })
+    # state.chat_history.append({
+    #     "role": "user",
+    #     "content": await PromptBuilder().build_prompt(message)
+    # })
     prepping_question_notifier.dismiss()
 
 
@@ -59,29 +59,34 @@ async def on_send_message(state: State) -> None:
 
 @ui.refreshable
 def MessagePane(state: State) -> None:
-    with ui.scroll_area().classes('w-full h-full'):
-        for message in state.displayed_chat_history:
-            if message["role"] == "user":
-                ChatMessage(message["content"], from_bot=False)
-            else:
-                ChatMessage(message["content"], from_bot=True)
+    with ui.column().classes('fit col'):
+        with ui.scroll_area().classes('h-full'):
+            for message in state.displayed_chat_history:
+                if message["role"] == "user":
+                    with ui.element('div').classes('w-2/3 self-center'):
+                        ChatMessage(message["content"], from_bot=False)
+                else:
+                    with ui.element('div').classes('w-2/3 self-center'):
+                        ChatMessage(message["content"], from_bot=True)
+
+def PromptPane(state: State) -> None:
+    with ui.card().classes('w-2/3 bg-white self-center'):
+        with ui.row(wrap=False).classes('w-full pt-0'):
+            ui.textarea() \
+                .bind_value(state, 'unsent_prompt') \
+                .classes('w-full') \
+                .props("color=white") \
+                .props("standout") \
+                .props("dense") \
+                .props('autogrow') \
+                .props("label='Ask a question...'") \
+                .props("stack-label")
+            ui.button('Send', 
+                on_click=lambda _: on_send_message(state),
+                color='black'
+            ).props("text-color='white'")
 
 def Chat(state: State) -> None:
-
-    with ui.column().classes('w-full h-[calc(100vh-2rem)]'):
+    with ui.column().classes('w-full h-full gap-0'):
         MessagePane(state)
-        with ui.card().classes('w-[calc(100vh*0.85)] absolute bottom-1 bg-white self-center'):
-            with ui.row(wrap=False).classes('w-full'):
-                ui.textarea() \
-                    .bind_value(state, 'unsent_prompt') \
-                    .classes('w-full') \
-                    .props("color=white") \
-                    .props("standout") \
-                    .props("dense") \
-                    .props('autogrow') \
-                    .props("label='Ask a question...'") \
-                    .props("stack-label")
-                ui.button('Send', 
-                    on_click=lambda _: on_send_message(state),
-                    color='black'
-                ).props("text-color='white'")
+        PromptPane(state)
--- src/main.py
+++ src/main.py
@@ -6,16 +6,26 @@ from Settings import Settings
 from State import State
 
 state = State()
+state.displayed_chat_history = [
+    {
+        "role": "user",
+        "content": "h\n\n\n\nhh\n\n\n\nhh\n\n\n\nhh\n\n\n\nh"
+    },
+    {
+        "role": "system",
+        "content": "h\n\n\n\nhh\n\n\n\nhh\n\n\n\nhh\n\n\n\nh"
+    }
+]
 
-with ui.column().classes('w-full h-[calc(100vh-2rem)]'):
+with ui.column().classes('w-full h-[calc(100vh-2rem)] overscroll-none'):
     with ui.tabs().classes('w-full') as tabs:
         ui.tab('addContent', label='Add Content')
         ui.tab('chat', label='Chat')
         ui.tab('settings', label='Settings')
-    with ui.tab_panels(tabs).classes('w-full'):
+    with ui.tab_panels(tabs).classes('w-full h-full'):
         with ui.tab_panel('addContent'):
             ContentLoaderMenu(state.content_loader)
-        with ui.tab_panel('chat').classes('w-full'):
+        with ui.tab_panel('chat').classes('w-full h-full'):
             Chat(state)
         with ui.tab_panel('settings').classes('w-full'):
             Settings(state)
@@ -23,6 +33,6 @@ with ui.column().classes('w-full h-[calc(100vh-2rem)]'):
 ui.run(
     title='Tarragon', 
     favicon='../T.ico', 
-    native=True, 
+    # native=True, 
     port=8081
 )
